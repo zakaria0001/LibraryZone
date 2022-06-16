8cf2c2461a108cb9103f14f717c3fb62550fe171
zakaria0001 <zakarianabil68@gmail.com>
2022-06-16 05:22:15 +0100

    Update settings.py

--- LibGestion/settings.py
+++ LibGestion/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-xtj+7n=_p@_$dskk15e5u4r+i^==!rejuujv@d8u95c7jhuk18
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['libraryzone.herokuapp.com']
 
 
 # Application definition
